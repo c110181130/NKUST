2183a62454e4900be86b8dac9acdf704c5f6ef6d
c110181130 <93572202+c110181130@users.noreply.github.com>
2021-11-09 16:42:09 +0800

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,8 @@
+public class Data {
+    public static void main(String[] args) {
+        float a = 1.75f;
+        int b = 72;
+        float BMI = b/(a*a);
+        System.out.println(BMI);
+    }
+}

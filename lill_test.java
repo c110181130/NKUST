95f2033b988c872ebd9ad3ddc12490432db50182
c110181130 <peixianwu101@gamil.com>
2021-12-21 13:20:05 +0800

    12/21work

--- lill_test.java
+++ lill_test.java
@@ -1,21 +1,22 @@
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-public class lill_test {
-        public static void main(String[] args) {
-            try {
-                Document doc = Jsoup.connect("https://rule.nkust.edu.tw/").get();
-                System.out.println(doc.title());
-                Elements newsHeadlines = doc.select("#Dyn_2_2 a");
-                for (Element headline : newsHeadlines){
-                    System.out.printf("%s\n%s\n", headline.attr("title"),headline.absUrl("href"));
-                }
-
-            } catch (Exception e) {
-            }
-        }
-    }
-
-
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//public class lill_test {
+//        public static void main(String[] args) {
+//            try {
+//                Document doc = Jsoup.connect("https://rule.nkust.edu.tw/").get();
+//                System.out.println(doc.title());
+//                Elements newsHeadlines = doc.select("#Dyn_2_2 a");
+//                for (Element headline : newsHeadlines){
+//                    System.out.printf("%s\n%s\n", headline.attr("title"),headline.absUrl("href"));
+//                }
+//
+//            } catch (Exception e) {
+//            }
+//        }
+//    }
+//
+//
+//
